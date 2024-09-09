ded9dc4ea9f8662f29dc8c811ffbcaabafb8cdab
Oliver Bestmann <oliver.bestmann@googlemail.com>
2024-09-09 10:32:10 +0200

    linux-asahi: 6.10.6-1 -> 6.10.7-2

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -86,7 +86,7 @@ let
     (linuxKernel.manualConfig rec {
       inherit stdenv lib;
 
-      version = "6.10.6-asahi";
+      version = "6.10.7-asahi";
       modDirVersion = version;
       extraMeta.branch = "6.10";
 
@@ -94,8 +94,8 @@ let
         # tracking: https://github.com/AsahiLinux/linux/tree/asahi-wip (w/ fedora verification)
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "asahi-6.10.6-1";
-        hash = "sha256-qm+0YYHehR2GP/MNAnTSPCBhb1vpnR50bbpfap74BRc=";
+        rev = "asahi-6.10.7-2";
+        hash = "sha256-R2oC5nxibugU3tLZ6wjR7azXsRgHna1iM9KnXJS05qo=";
       };
 
       kernelPatches = [
@@ -119,4 +119,3 @@ let
 
   linux-asahi = (callPackage linux-asahi-pkg { });
 in lib.recurseIntoAttrs (linuxPackagesFor linux-asahi)
-
