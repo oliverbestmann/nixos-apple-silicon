7aad69158fc1b5bbbddac19040b6aae14daaa35c
Yureka <yuka@yuka.dev>
2025-10-20 09:46:02 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/d3baaf296366efdea6737124f05b65d1cf25fa7c' (2025-10-06)
      → 'github:NixOS/nixpkgs/5e2a59a5b1a82f89f2c7e598302a9cacebb72a67' (2025-10-19)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Apple Silicon support for NixOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-compat.url = "github:nix-community/flake-compat";
   };
 
