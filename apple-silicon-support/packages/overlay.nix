c85deb2dfed564c003145a2e7b22a3b41e533022
Oliver Bestmann <oliver.bestmann@googlemail.com>
2025-11-28 20:07:29 +0100

    add linux-asahi-mini

--- apple-silicon-support/packages/overlay.nix
+++ apple-silicon-support/packages/overlay.nix
@@ -1,4 +1,5 @@
 final: prev: {
   linux-asahi = final.callPackage ./linux-asahi { };
   uboot-asahi = final.callPackage ./uboot-asahi { };
+  linux-asahi-mini = final.callPackage ./linux-asahi-mini { };
 }
